b496a4a7f052e25bef65c8176bbd75a726f0a036
Vyacheslav Shcherbinin <gentslava@mail.ru>
2023-10-06 15:20:24 +0700

    Only .wasm files

--- scripts/folder_sorting.sh
+++ scripts/folder_sorting.sh
@@ -1,4 +1,4 @@
-for f in `ls ./tests | grep .wasm | sed -e 's/\(.wasm\)*$//g'`;
+for f in `ls ./tests | grep \.wasm$ | sed -e 's/\(.wasm\)*$//g'`;
 do
   mkdir -p ./tests/$f;
   mv -i ./tests/$f.* ./tests/$f;
--- scripts/wasm_to_wat.sh
+++ scripts/wasm_to_wat.sh
@@ -1,4 +1,4 @@
-for f in `find . -type f | grep .wasm`;
+for f in `find . -type f | grep \.wasm$`;
 do
   wasm2wat $f -o ${f/.wasm/.wat};
 done
